323e57c00df4e7da734853893262e77a1a3ca457
gitarecy <98228087+gitarecy@users.noreply.github.com>
2022-07-18 01:44:53 +0200

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,13 +8,11 @@
 void print_number(int n)
 {
 	int res, temp, expo;
-	
+
 	expo = 1;
 /*Check negatives*/
 	if (n >= 0)
-	{
 		res = n * -1;
-	}
 	else
 	{
 		res = n;
@@ -27,13 +25,10 @@ void print_number(int n)
 	{
 		expo *= 10;
 		temp /= 10;
-		printf("expo: %d, temp: %d, orig: %d\n", expo, temp, res);
 	}
 /*Main */
 	while (expo >= 1)
 	{
-
-		printf("expo: %d, res: %d\n", expo, ((res /expo) % 10) * -1);
 		_putchar(((res / expo) % 10) * -1 + '0');
 		expo /= 10;
 	}
